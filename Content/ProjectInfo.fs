11489d74937959e08b84b7efafffe4fa1eed3a23
Kevin Schneider <schneider.kev@outlook.de>
2022-03-08 15:05:26 +0100

    Add template

---
+++ Content/ProjectInfo.fs
@@ -0,0 +1,34 @@
+﻿module ProjectInfo
+
+open Fake.Core
+
+let project = "{PROJECTNAME}"
+
+let testProjects = 
+    [
+        // add relative paths (from project root) to your testprojects here
+    ]
+
+let solutionFile  = $"{project}.sln"
+
+let configuration = "Release"
+
+let gitOwner = "{GITOWNER}"
+
+let gitHome = $"https://github.com/{gitOwner}"
+
+let projectRepo = $"https://github.com/{gitOwner}/{project}"
+
+let pkgDir = "pkg"
+
+let release = ReleaseNotes.load "RELEASE_NOTES.md"
+
+let stableVersion = SemVer.parse release.NugetVersion
+
+let stableVersionTag = (sprintf "%i.%i.%i" stableVersion.Major stableVersion.Minor stableVersion.Patch )
+
+let mutable prereleaseSuffix = ""
+
+let mutable prereleaseTag = ""
+
+let mutable isPrerelease = false
